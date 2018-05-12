90e6aa5726e2e6871f26296abfe694c49f2804ef
José Marcondes do Nascimento Junior <junior3917@gmail.com>
2018-05-12 15:07:40 -0300

    profile com docker adicionado

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 docker build -t pos-banco/db ./postgres
 docker run --name pos-banco-db -p 5433:5432 -d pos-banco/db
 
-mvn clean package
+mvn clean package -Pdocker
 
 docker build -t pos-banco/app .
 docker run --name pos-banco-app -p 8080:8080 -p 4848:4848 --link pos-banco-db:postgres-db -d pos-banco/app
